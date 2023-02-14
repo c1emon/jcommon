c7194015e805ed11bd3cbc32297dbebf2ce4771b
clemon <cjw7360chen@gmail.com>
2023-02-14 23:20:00 +0800

    upgrade deps and refactor gradle

--- src/main/java/icu/clemon/common/http/CommonErrorController.java
+++ src/main/java/icu/clemon/common/http/CommonErrorController.java
@@ -1,14 +1,13 @@
 package icu.clemon.common.http;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @RequiredArgsConstructor
