1c24ca3da63009ced69c464b0af908fdd1a6c3a0
clemon <cjw7360chen@gmail.com>
2023-02-14 22:47:30 +0800

    refactor gradle and add hello controller

--- src/main/java/icu/clemon/template/controller/TestController.java
+++ src/main/java/icu/clemon/template/controller/TestController.java
@@ -1,12 +1,15 @@
 package icu.clemon.template.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Test {
+@Slf4j
+public class TestController {
     @RequestMapping("/hello")
     public String Hello() {
+        log.info("log from hello");
         return "hello";
     }
 }
