7c6f4091ba70bdb5f3957cdfcccaf7edcbbd4224
clemon <cjw7360chen@gmail.com>
2023-02-17 23:04:08 +0800

    disable _class field

--- src/main/java/icu/clemon/common/mongodb/MongoAutoConfiguration.java
+++ src/main/java/icu/clemon/common/mongodb/MongoAutoConfiguration.java
@@ -1,9 +1,24 @@
 package icu.clemon.common.mongodb;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @AutoConfiguration
 public class MongoAutoConfiguration {
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory factory, MongoMappingContext context) {
+        DbRefResolver resolver = new DefaultDbRefResolver(factory);
+        MappingMongoConverter converter = new MappingMongoConverter(resolver, context);
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return converter;
+    }
 
 }
