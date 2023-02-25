88e67d93c01af0ad261fec1e33ca9bff0d9a2d73
clemon <cjw7360chen@gmail.com>
2023-02-25 22:28:25 +0800

    tested Enumerator

--- src/main/java/icu/clemon/common/types/Enumerator.java
+++ src/main/java/icu/clemon/common/types/Enumerator.java
@@ -2,11 +2,10 @@ package icu.clemon.common.types;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import icu.clemon.common.json.EnumeratorDeserializer;
-import icu.clemon.common.json.EnumeratorSerializer;
+import icu.clemon.common.json.EnumeratorSerializers;
 
-@JsonDeserialize(using = EnumeratorDeserializer.class)
-@JsonSerialize(using = EnumeratorSerializer.class)
+@JsonDeserialize(using = EnumeratorSerializers.Deserializer.class)
+@JsonSerialize(using = EnumeratorSerializers.Serializer.class)
 public interface Enumerator {
     Integer getId();
     String getDescription();
