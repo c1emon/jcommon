cbcfd8ecf1909d9e321162ec2388f7c196778bfd
clemon <cjw7360chen@gmail.com>
2024-08-28 22:01:56 +0800

    remove some param...

--- src/main/java/icu/clemon/jcommon/utils/anytree/Node.java
+++ src/main/java/icu/clemon/jcommon/utils/anytree/Node.java
@@ -1,40 +1,18 @@
 package icu.clemon.jcommon.utils.anytree;
 
-import lombok.Data;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Node<T> {
 
-    private String name;
     private T data;
 
-    private Node<T> parent = null;
     private List<Node<T>> children;
-
-
-    public Node(String name, Node<T> parent, T data) {
-        this.name = name;
-        this.data = data;
-        if (parent != null) {
-            this.parent = parent;
-            parent.addChild(this);
-        }
-    }
-
-    public Node(String name, Node<T> parent) {
-        this(name, parent, null);
-    }
-
-    public Node(String name, T data) {
-        this(name, null, data);
-    }
-
-    public Node(String name) {
-        this(name, null, null);
-    }
 
     public void addChild(Node<T> child) {
         if (children == null) {
@@ -42,9 +20,4 @@ public class Node<T> {
         }
         children.add(child);
     }
-
-    public boolean isRoot() {
-        return parent == null;
-    }
-
 }
